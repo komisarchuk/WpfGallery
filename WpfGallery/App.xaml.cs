159f375c0813559f3178c7e04d1ac768f84341bc
komisarchuk <komisarchuk@gmail.com>
2019-10-06 09:41:34 +0300

    Gallery project start

---
+++ WpfGallery/App.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfGallery
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        void OnApplicationStartup(object sender, StartupEventArgs args)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+        }
+
+    }
+}
---
+++ WpfGallery/Data.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace WpfGallery
+{
+    public class Photo
+    {
+        private string _path;
+        private Uri _source;
+        private BitmapFrame _image;
+
+        public Photo(string path)
+        {
+            _path = path;
+            _source = new Uri(path);
+            _image = BitmapFrame.Create(_source);
+        }
+        public override string ToString()
+        {
+            return _source.ToString();
+        }
+        public BitmapFrame Image { get { return _image; } }
+    }
+
+    public class PhotoCollection : ObservableCollection<Photo>
+    {
+        private DirectoryInfo _directory;
+        public PhotoCollection() { }
+        public PhotoCollection(string path) : this(new DirectoryInfo(path)) { }
+
+        public PhotoCollection(DirectoryInfo directory)
+        {
+            _directory = directory;
+            Update();
+        }
+        public string Path
+        {
+            get { return _directory.FullName; }
+            set
+            {
+                _directory = new DirectoryInfo(value);
+                Update();
+            }
+        }
+        public DirectoryInfo Directory
+        {
+            get { return _directory; }
+            set
+            {
+                _directory = value;
+                Update();
+            }
+        }
+        private void Update()
+        {
+            this.Clear();
+            try
+            {
+                foreach(FileInfo f in _directory.GetFiles("*.jpg"))
+                {
+                    Add(new Photo(f.FullName));
+                }
+            }
+            catch(DirectoryNotFoundException)
+            {
+                MessageBox.Show("No such directory");
+            }
+        }
+    }
+
+}
